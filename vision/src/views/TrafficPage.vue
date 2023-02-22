cc714eaa913530a7b04688ad2a13a9548ac3d212
江鹏利 <845415120@qq.com>
2023-02-22 09:30:01 +0800

    提交

---
+++ vision/src/views/TrafficPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="com-page">
+    <traffic></traffic>
+  </div>
+</template>
+
+<script>
+import Traffic from '../components/Traffic.vue'
+export default {
+  data () {
+    return {}
+  },
+  methods: {},
+  components: {
+    traffic: Traffic,
+    Traffic
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
